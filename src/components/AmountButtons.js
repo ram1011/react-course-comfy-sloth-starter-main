a803b6ad21374aa78ac9839cf1129bd1fa6587d4
ram1011 <ramece0404@gmail.com>
2021-12-09 16:16:29 +1100

    Amount
    Amount btns

--- src/components/AmountButtons.js
+++ src/components/AmountButtons.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import { FaPlus, FaMinus } from 'react-icons/fa'
 
 const AmountButtons = () => {
-  return <h4>amount buttons </h4>
+  return <h4>Amount buttons </h4>
 }
 
 const Wrapper = styled.div`
